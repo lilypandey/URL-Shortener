7f98f0c61da3830aed2b5ef3630c40ae72e886fc
Lily Pandey <panlily2002@gmail.com>
2025-06-01 10:13:52 +0530

    added db entities

---
+++ url-shortener-sb/src/main/java/com/url/shortener/models/UrlMapping.java
@@ -0,0 +1,25 @@
+package com.url.shortener.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String original_url;
+    private String short_url;
+    private int clickCount = 0;
+    private LocalDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents;
+}
