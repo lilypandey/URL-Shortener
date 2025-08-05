57a564f2ce488e03d4540491c68aa8c9ac09ad35
Lily Pandey <panlily2002@gmail.com>
2025-08-05 23:16:12 +0530

    jwt

--- url-shortener-sb/src/main/java/com/url/shortener/models/User.java
+++ url-shortener-sb/src/main/java/com/url/shortener/models/User.java
@@ -11,7 +11,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
+    private String username;
     private String password;
     private String role = "ROLE_USER";
-    private String username;
 }
