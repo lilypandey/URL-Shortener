f61fda44d020157fdbd6eeb97e4a19969f4aab3b
Lily Pandey <panlily2002@gmail.com>
2025-09-05 19:39:26 +0530

    register

--- url-shortener-frontend/src/App.jsx
+++ url-shortener-frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Router>
+      <NavBar />
+      <Toaster position='bottom-center'/>
+      <Routes>
+        <Route path = '/' element={<LandingPage />}/>
+        <Route path = '/about' element={<AboutPage />}/>
+        <Route path = '/register' element={<RegisterPage />}/>
+      </Routes>
+      <Footer />
+      </Router>
     </>
   )
 }
