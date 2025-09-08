b263cdf8c47e0c2d7cf4e67dac2de9a5bac84c0b
Lily Pandey <panlily2002@gmail.com>
2025-09-08 11:59:46 +0530

    frontend

---
+++ url-shortener-frontend/src/AppRouter.jsx
@@ -0,0 +1,45 @@
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import ShortenUrlPage from "./components/ShortenUrlPage";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
+import AboutPage from "./components/AboutPage";
+import RegisterPage from "./components/RegisterPage";
+import LoginPage from "./components/LoginPage";
+import DashboardLayout from "./components/Dashboard/DashboardLayout";
+import PrivateRoute from "./PrivateRoute";
+
+
+// <PrivateRoute publicPage={true}>
+//      <RegisterPage />
+// </PrivateRoute>
+
+const AppRouter = () => {
+
+    return (
+        <>
+        <NavBar />
+        <Toaster position='bottom-center'/>
+        <Routes>
+            <Route path = '/' element={<LandingPage />}/>
+            <Route path = '/about' element={<AboutPage />}/>
+            <Route path="/register" element={<PrivateRoute publicPage={true}><RegisterPage /></PrivateRoute>} />
+            <Route path="/login" element={<PrivateRoute publicPage={true}><LoginPage /></PrivateRoute>} />
+            <Route path = '/dashboard' element={<PrivateRoute publicPage={false}><DashboardLayout /></PrivateRoute>}/>
+        </Routes>
+        <Footer />
+      </>
+    );
+}
+
+
+export default AppRouter;
+
+export const SubDomainRouter = () => {
+    return (
+        <Routes>
+          <Route path="/:url" element={<ShortenUrlPage />} />
+        </Routes>
+    )
+}
